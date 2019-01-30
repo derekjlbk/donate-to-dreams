be1100d11899cc35f3742fdea8d1114e00feb736
Derek Johnston <derekjlbk@gmail.com>
2019-01-30 10:23:30 -0600

    Adding primary user interface for the manager.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './bootstrap.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <h1>Donate to Dreams</h1>
+        <ul className="nav">
+          <li className="nav-item">
+            <a href="#" className="nav-link">Launch Display</a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">Import Data File</a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">Export Data File</a>
+          </li>
+        </ul>
+        <hr />
+        <main>
+          <section>
+            <form>
+              <div className="form-group">
+                <label htmlFor="bid-number">Bid Number</label>
+                <input type="number" className="form-control" id="bid-number" placeholder="001" required/>
+              </div>
+              <div className="form-group">
+                <label htmlFor="bid-amount">Bid Amount</label>
+                <select className="form-control" id="bid-amount" required>
+                  <option value="1000">$1,000</option>
+                  <option value="100">$100</option>
+                  <option value="10">$10</option>
+                  <option value="1">$1</option>
+                </select>
+              </div>
+              <div className="form-group">
+                <input type="submit" className="form-control btn btn-primary" value="Place Bid" />
+              </div>
+            </form>
+          </section>
+          <table className="table table">
+            <thead>
+              <tr>
+                <th>Bid Number</th>
+                <th>Name</th>
+                <th>Display Name</th>
+                <th>Gift Amount</th>
+                <th>...</th>
+              </tr>
+            </thead>
+          </table>
+        </main>
       </div>
     );
   }
