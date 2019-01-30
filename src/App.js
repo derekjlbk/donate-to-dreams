d41889f3abda9b996bd30565cbc3a048ea95c35a
Derek Johnston <derekjlbk@gmail.com>
2019-01-30 14:20:52 -0600

    Creating new bids and updating database.

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
     this.exportBtnClicked = this.exportBtnClicked.bind(this)
     this.updateBidderData = this.updateBidderData.bind(this)
     this.renderBidderTable = this.renderBidderTable.bind(this)
+    this.placeNewBid = this.placeNewBid.bind(this)
 
     firebase.database().ref("bidders").on("value", this.updateBidderData)
   }
@@ -94,6 +95,56 @@ class App extends Component {
     })
   }
 
+  placeNewBid(event) {
+    event.preventDefault()
+    console.log("Placing a new bid")
+
+    const bidNumber = document.getElementById("bid-number").value
+    const amount = document.getElementById("bid-amount").value
+
+    var bidder = {}
+
+    // Get the bidder from the bid number
+    for (var i = 0; i < this.state.bidders.length; i++) {
+      if (this.state.bidders[i]["Paddle ID"] == bidNumber) {
+        bidder = this.state.bidders[i]
+        break
+      }
+    }
+
+    if (Object.entries(bidder).length == 0) {
+      console.error("Bidder Number not found")
+      alert("Bid number " + bidNumber + " was not found.")
+      return
+    }
+
+    console.log("Bidder " + bidder["Name"] + " gave $" + amount)
+
+    let bidderData = {}
+
+    // Fetch the bidder with the correct number.
+    firebase.database().ref("bidders/" + bidNumber).once("value")
+    .then((snapshot) => {
+      bidderData = snapshot.val()
+      let currentAmount = Number(bidderData["Donate to Dreams"])
+      let newAmount = currentAmount + Number(amount)
+
+      bidderData["Donate to Dreams"] = newAmount
+
+      return firebase.database().ref("bidders/" + bidNumber).set(bidderData)
+    })
+    .then(() => {
+      let bid = {
+        name: bidderData["Name"],
+        amount: amount
+      }
+
+      return firebase.database().ref("bids").push(bid)
+    })
+
+    document.getElementById("bid-number").value = ""
+  }
+
   render() {
     return (
       <div className="container">
@@ -112,18 +163,19 @@ class App extends Component {
         <hr />
         <main>
           <section>
-            <form>
+            <form onSubmit={this.placeNewBid}>
               <div className="form-group">
                 <label htmlFor="bid-number">Bid Number</label>
-                <input type="number" className="form-control" id="bid-number" placeholder="001" required/>
+                <input type="number" className="form-control" id="bid-number" placeholder="XXX" required/>
               </div>
               <div className="form-group">
                 <label htmlFor="bid-amount">Bid Amount</label>
                 <select className="form-control" id="bid-amount" required>
                   <option value="1000">$1,000</option>
+                  <option value="500">$500</option>
                   <option value="100">$100</option>
-                  <option value="10">$10</option>
-                  <option value="1">$1</option>
+                  <option value="50">$50</option>
+                  <option value="25">$25</option>
                 </select>
               </div>
               <div className="form-group">
@@ -136,7 +188,7 @@ class App extends Component {
               <tr>
                 <th>Bid Number</th>
                 <th>Name</th>
-                <th>Gift Amount</th>
+                <th>D2D Amount</th>
                 <th>...</th>
               </tr>
             </thead>
